38f2bae19244169742e2e8bc92aa9711a21382ff
Matheus Souza <szmatheussouza@gmail.com>
2024-08-23 20:33:48 -0300

    Iniciado caixa eletronico

--- src/main/java/org/example/CaixaEletronico.java
+++ src/main/java/org/example/CaixaEletronico.java
@@ -1,5 +1,10 @@
 package org.example;
 
 public class CaixaEletronico {
-
+    public static void main(String[] args){
+        int opcao = 0;
+        do {
+            System.out.println("teste");
+        } while(opcao == 0);
+    }
 }
